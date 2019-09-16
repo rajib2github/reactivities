909c11f6a3e3e1017563d7d52ed5bb376d7099d4
rajib <rajib2u@gmail.com>
2019-09-16 21:50:31 +0530

    Styling added for activity List

--- client-app/src/features/dashboard/ActivityList.tsx
+++ client-app/src/features/dashboard/ActivityList.tsx
@@ -1,56 +1,28 @@
-import React, { useContext } from "react";
-import { Item, Button, Label, Segment } from "semantic-ui-react";
+import React, { useContext, Fragment } from "react";
+import { Item, Label } from "semantic-ui-react";
 import { observer } from "mobx-react-lite";
 import ActivityStore from "../../app/stores/activitystore";
-import { Link } from "react-router-dom";
-
+import ActivityListItem from "./ActivityListItem";
 
 const ActivityList: React.FC = () => {
   const activityStore = useContext(ActivityStore);
-  const {
-    activityByDate,    
-    deleteActivity,
-    submitting,
-    target
-  } = activityStore;
+  const { activityByDate } = activityStore;
 
   return (
-    <Segment clearing>
-      <Item.Group divided>
-        {activityByDate.map(activity => (
-          <Item key={activity.id}>
-            <Item.Content>
-              <Item.Header as="a">{activity.title}</Item.Header>
-              <Item.Meta>{activity.date}</Item.Meta>
-              <Item.Description>
-                <div>{activity.descrition}</div>
-                <div>
-                  {activity.city}, {activity.venue}
-                </div>
-              </Item.Description>
-              <Item.Extra>
-                <Button
-                  as={Link}
-                  to={`/activities/${activity.id}`}
-                  floated="right"
-                  content="View"
-                  color="blue"
-                />
-                <Button
-                  name={activity.id}
-                  loading={target === activity.id && submitting}
-                  onClick={e => deleteActivity(e, activity.id)}
-                  color="red"
-                  floated="right"
-                  content="Delete"
-                />
-                <Label basic content={activity.category} />
-              </Item.Extra>
-            </Item.Content>
-          </Item>
-        ))}
-      </Item.Group>
-    </Segment>
+    <Fragment>
+      {activityByDate.map(([group, activities]) => (
+        <Fragment key={group}>
+          <Label size="large" color="blue">
+            {group}
+          </Label>
+          <Item.Group divided>
+            {activities.map(activity => (
+              <ActivityListItem key={activity.id} activity={activity} />
+            ))}
+          </Item.Group>
+        </Fragment>
+      ))}
+    </Fragment>
   );
 };
 
